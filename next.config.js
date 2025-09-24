2757ba9d6aaffd0d8aa0b68c4fed3997157d515a
Birthpakornpong <144552444+Birthpakornpong@users.noreply.github.com>
2025-09-24 16:11:30 +0700

    edit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   basePath: '',
   assetPrefix: '',
-  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
