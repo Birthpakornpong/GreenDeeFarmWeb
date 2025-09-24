0def9279dde8ff781f0657eca3d829cb1235fb5e
Birthpakornpong <144552444+Birthpakornpong@users.noreply.github.com>
2025-09-24 16:01:45 +0700

    Add clean next.config.js without secrets

---
+++ next.config.js
@@ -0,0 +1,14 @@
+﻿const withPlugins = require('next-compose-plugins')
+
+const plugins = []
+
+const nextConfig = {
+  env: {
+    VERSION: '1.0',
+  },
+  experimental: {
+    outputStandalone: true,
+  },
+}
+
+module.exports = withPlugins(plugins, nextConfig)
